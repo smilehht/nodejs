e1e1bf3ce7fa2ba14ce69452f2759f578cb499ad
huhongtao <956184070@qq.com>
2017-06-27 09:51:09 +0800

    webpack.config

--- projects/Blog/webpack.config.js
+++ projects/Blog/webpack.config.js
@@ -3,10 +3,49 @@
 *   @author huhongtao
 */
 
+'use strict';
+
 let webpack = require('webpack');
 
 module.exports = {
     entry: {
-        home: 
+        home: __dirname + '/resource/home/index.js'
+    },
+
+    output: {
+        path: __dirname + '/public/js',
+        filename: '[name].bundle.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.(css|scss)/,
+                loader: 'style!css!sass'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.', '.js'],
+        alias: {
+
+        }
     }
+
+    // plugins: [
+    //     new webpack.optimize.UglifyJsPlugin({
+    //         compress: {
+    //             warning: true
+    //         }
+    //     })
+    // ]
 };
