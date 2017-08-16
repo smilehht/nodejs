5ede3d30a0000c6891624038a6dc4c95e5541f28
huhongtao <956184070@qq.com>
2017-08-16 09:59:45 +0800

    components

---
+++ projects/Blog/redux/components/app.js
@@ -0,0 +1,11 @@
+import AddTodo from  '../containers/addtodo.js';
+import VisibleTodoList from  '../containers/visibleTodoList.js';
+import Footer from './footer.js';
+
+const App = () => {
+    <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+    </div>
+}
